0b705560d0cdca4a92e0a0fe17d5aa0437c31b03
樊康鹏 <15936219505@163.com>
2016-08-18 13:43:27 +0800

    Initial Commit

---
+++ cocoapodDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cocoapodDemo
+//
+//  Created by 樊康鹏 on 16/8/18.
+//  Copyright © 2016年 Karen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
